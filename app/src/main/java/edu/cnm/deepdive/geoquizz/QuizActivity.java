f4c20682551fd8bb4424ab944415d0e16c7c970f
Thomas Herrera <rayherrera90@outlook.com>
2019-02-06 19:59:08 -0700

    Fixed landscape error.

--- app/src/main/java/edu/cnm/deepdive/geoquizz/QuizActivity.java
+++ app/src/main/java/edu/cnm/deepdive/geoquizz/QuizActivity.java
@@ -2,6 +2,7 @@ package edu.cnm.deepdive.geoquizz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnContextClickListener;
 import android.widget.Button;
@@ -10,12 +11,15 @@ import android.widget.Toast;
 
 public class QuizActivity extends AppCompatActivity {
 
+  private static final String TAG = "QuizActivity";
+  private static final String KEY_INDEX = "Index";
+
   private Button mTrueButton;
   private Button mFalseButton;
   private Button mNextButton;
   private TextView mQuestionTextView;
 
-  private Question[] mQuestionBank = new Question[] {
+  private Question[] mQuestionBank = new Question[]{
       new Question(R.string.question_austrailia, true),
       new Question(R.string.question_oceans, true),
       new Question(R.string.question_mideast, false),
@@ -28,7 +32,12 @@ public class QuizActivity extends AppCompatActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+    Log.d(TAG, "onCreate(Bundle) called");
     setContentView(R.layout.activity_quiz);
+
+    if(savedInstanceState != null) {
+      mCurrentIndex = savedInstanceState.getInt(KEY_INDEX, 0);
+    }
 
     mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
 
@@ -43,15 +52,15 @@ public class QuizActivity extends AppCompatActivity {
     mFalseButton.setOnClickListener(new View.OnClickListener() {
 
       @Override
-          public void onClick(View v) {
-            checkAnswer(false);
+      public void onClick(View v) {
+        checkAnswer(false);
       }
     });
 
     mNextButton = (Button) findViewById(R.id.next_buuton);
     mNextButton.setOnClickListener(new View.OnClickListener() {
       @Override
-      public void  onClick(View v) {
+      public void onClick(View v) {
         mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
         updateQuestion();
 
@@ -61,10 +70,49 @@ public class QuizActivity extends AppCompatActivity {
     updateQuestion();
   }
 
-  private  void updateQuestion() {
+  @Override
+  public void onStart() {
+    super.onStart();
+    Log.d(TAG, "onStart() called");
+  }
+
+  @Override
+  public void onResume() {
+    super.onResume();
+    Log.d(TAG, "onResume() called");
+  }
+
+  @Override
+  public void onPause() {
+    super.onPause();
+    Log.d(TAG, "onPause() called");
+  }
+
+  @Override
+  public void onSaveInstanceState(Bundle savedInstanceState) {
+    super.onSaveInstanceState(savedInstanceState);
+    Log.i(TAG, "onSaveInstanceState");
+    savedInstanceState.putInt(KEY_INDEX, mCurrentIndex);
+  }
+
+  @Override
+  public void onStop() {
+    super.onStop();
+    Log.d(TAG, "onStop() called");
+  }
+
+  @Override
+  public void onDestroy() {
+
+    super.onDestroy();
+    Log.d(TAG, "onDestroy() called");
+  }
+
+  private void updateQuestion() {
     int question = mQuestionBank[mCurrentIndex].getTextResId();
     mQuestionTextView.setText(question);
   }
+
   private void checkAnswer(boolean userPressedTrue) {
     boolean answerIsTrue = mQuestionBank[mCurrentIndex].isAnswerTrue();
 
@@ -75,7 +123,7 @@ public class QuizActivity extends AppCompatActivity {
     } else {
       messageResId = R.string.incorrect_toast;
     }
-    Toast.makeText(this,messageResId,Toast.LENGTH_SHORT)
+    Toast.makeText(this, messageResId, Toast.LENGTH_SHORT)
         .show();
   }
 }
